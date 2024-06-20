b3e8e2bacbaeca00c086963083cdacb998939c08
bambooOctopus <bamboooctopus@gmail.com>
2024-06-20 16:01:35 -0400

    month flipping functionality complete

--- src/components/monthCalendar/MonthCalendarComponent.jsx
+++ src/components/monthCalendar/MonthCalendarComponent.jsx
@@ -1,22 +1,123 @@
 import './month-styles.css'
+import { useState, useEffect } from 'react';
+import { DateTime } from 'luxon';
 
-export const MonthCalendarComponent = () => {
+export const MonthCalendarComponent = () => {    
+    
+    const [ monthYearString, setMonthYearString ] = useState("") 
+    const [ daysArray, setDaysArray ] = useState([])  
+
+    const numberOfDaysConversion = {
+        "Mon": 0,
+        "Tue": 1,
+        "Wed": 2,
+        "Thu": 3,
+        "Fri": 4,
+        "Sat": 5,
+        "Sun": 6
+
+    }
 
     const previousSymbol = "<"
     const nextSymbol = ">"
 
-    
+    const mappedDaysArray = daysArray.map(day => {        
 
-    return (
-        <div className='month-calendar-container'>
-            <div className='month-nav'>
-                <button>{previousSymbol}</button>
-                <p>June 2024</p>
-                <button>{nextSymbol}</button>
-            </div>
+        return (
+            <>
+            {day != "x" ? 
+                <div className='day'><p>{day}</p></div>
+            :
+                <div className='non-day'></div>}            
+            </>
+            
+        )
+    })
 
-            <div className='month-days-container'>
-                <div className='day-name-header'>
+    const handlePreviousMonthClick = (event) => {
+        event.preventDefault()       
+
+        // parse monthYearString into DateTime        
+        let dt = DateTime.fromFormat(monthYearString, "MMMM yyyy")
+
+        // subtract one month from luxon date
+        let newDt = dt.minus({month: 1})      
+        
+
+        // make new monthYearString from newDt
+        let month = newDt.monthLong
+        let year = newDt.year
+        let newMonthDayString = month + " " + year        
+
+        // setMonthYearString to new date
+        setMonthYearString(newMonthDayString)
+
+        // setNewCalendarDays
+        let newCalendarDays = setCalendarDays({string: newMonthDayString})        
+        setDaysArray(newCalendarDays)
+        
+    }
+
+    const handleNextMonthClick = (event) => {
+        event.preventDefault()        
+
+        // parse monthYearString into DateTime        
+        let dt = DateTime.fromFormat(monthYearString, "MMMM yyyy")
+
+        // add one month to luxon date
+        let newDt = dt.plus({month: 1})      
+        
+
+        // make new monthYearString from newDt
+        let month = newDt.monthLong
+        let year = newDt.year
+        let newMonthDayString = month + " " + year        
+
+        // setMonthYearString to new date
+        setMonthYearString(newMonthDayString)
+
+        // setNewCalendarDays
+        let newCalendarDays = setCalendarDays({string: newMonthDayString})        
+        setDaysArray(newCalendarDays)
+
+    }
+
+    const setCalendarDays = ({string}) => {        
+        let dayArray = []
+        // get current month in use from monthYearString
+        let dt = DateTime.fromFormat(string, "MMMM yyyy")
+
+        // find out how many days in the month
+        let daysInMonth = dt.daysInMonth
+
+        // find day of week of first day in month       
+        let firstDate = dt.set({day: 1})
+        let firstDayName = firstDate.weekdayShort
+        // use numberOfDaysConversion to find out how many days
+        // to pad the front of the month with
+        let emptyDayNumber = numberOfDaysConversion[firstDayName]
+
+        // push empty days to dayArray
+        for (let i = 0; i < emptyDayNumber; i++) {
+            dayArray.push("x")
+        }
+        
+        // push this months dates to day array; start counter at 1
+        for (let i = 1; i <= daysInMonth; i++) {            
+            dayArray.push(i)
+        }
+
+        for (let i = 0; dayArray.length < 35; i++) {
+            dayArray.push("x")
+        }
+
+        return dayArray
+
+    }
+
+    const DayNameHeader = () => {
+        return (
+            <div className='day-name-header'>
                     <p className="day-name">mon</p>
                     <p className="day-name">tues</p>
                     <p className="day-name">weds</p>
@@ -24,44 +125,35 @@ export const MonthCalendarComponent = () => {
                     <p className="day-name">fri</p>
                     <p className="day-name">sat</p>
                     <p className="day-name">sun</p>                    
-                </div>
-                <div className='days-grid'>
-                    <div className="day">27</div>
-                    <div className="day">28</div>
-                    <div className="day">29</div>
-                    <div className="day">30</div>
-                    <div className="day">31</div>
-                    <div className="day">1</div>
-                    <div className="day">2</div>
-                    <div className="day">3</div>
-                    <div className="day">4</div>
-                    <div className="day">5</div>
-                    <div className="day">6</div>
-                    <div className="day">7</div>
-                    <div className="day">8</div>
-                    <div className="day">9</div>
-                    <div className="day">10</div>
-                    <div className="day">11</div>
-                    <div className="day">12</div>
-                    <div className="day">13</div>
-                    <div className="day">14</div>
-                    <div className="day">15</div>
-                    <div className="day">16</div>
-                    <div className="day">17</div>
-                    <div className="day">18</div>
-                    <div className="day">19</div>
-                    <div className="day">20</div>
-                    <div className="day">21</div>
-                    <div className="day">22</div>
-                    <div className="day">23</div>
-                    <div className="day">24</div>
-                    <div className="day">25</div>
-                    <div className="day">26</div>
-                    <div className="day">27</div>
-                    <div className="day">28</div>
-                    <div className="day">29</div>
-                    <div className="day">30</div>
-                </div>
+            </div>
+        )
+    } 
+
+    useEffect(() => {
+        const dt = DateTime.now()
+        
+        const monthName = dt.monthLong
+        const year = dt.year
+        const string = monthName + " " + year
+        let daysArray = setCalendarDays({string: string})        
+        setDaysArray(daysArray)
+        setMonthYearString(string)
+    }, [])
+    
+
+    
+
+    return (
+        <div className='month-calendar-container'>
+            <div className='month-nav'>
+                <button onClick={handlePreviousMonthClick} className='arrow-button'>{previousSymbol}</button>
+                <p>{monthYearString}</p>
+                <button onClick={handleNextMonthClick} className='arrow-button'>{nextSymbol}</button>
+            </div>
+
+            <div className='month-days-container'>
+                <DayNameHeader />
+                <div className='days-grid'>{mappedDaysArray}</div>             
             </div>
             
         </div>
